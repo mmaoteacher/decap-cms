ea0181ba101fa6ae8df1db352823e31476ce2a9c
Mmao <mmaoteacher@gmail.com>
2026-08-25 09:16:15 +0800

    fix(types): alias imported CMS interface to prevent merged declaration error

--- packages/decap-cms-app/index.d.ts
+++ packages/decap-cms-app/index.d.ts
@@ -1,8 +1,8 @@
 declare module 'decap-cms-app' {
   export * from 'decap-cms-core';
-  import type { CMS } from 'decap-cms-core';
+  import type { CMS as CmsInterface } from 'decap-cms-core';
 
-  export const DecapCmsApp: CMS;
+  export const DecapCmsApp: CmsInterface;
 
   export default DecapCmsApp;
 }
--- packages/decap-cms/index.d.ts
+++ packages/decap-cms/index.d.ts
@@ -1,8 +1,8 @@
 declare module 'decap-cms' {
   export * from 'decap-cms-core';
-  import type { CMS } from 'decap-cms-core';
+  import type { CMS as CmsInterface } from 'decap-cms-core';
 
-  export const CMS: CMS;
+  export const CMS: CmsInterface;
 
   export default CMS;
 }
